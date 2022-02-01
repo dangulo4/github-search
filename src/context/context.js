e409c60ab7f773a7ef323918d60be2dacaeee286
Daniel Angulo <dangulo4@gmail.com>
2022-01-31 18:36:33 -0800

    added charts with navbar

--- src/context/context.js
+++ src/context/context.js
@@ -13,26 +13,44 @@ const GithubContext = React.createContext()
 const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser)
   const [repos, setRepos] = useState(mockRepos)
-  const [followers, setFollwers] = useState(mockFollowers)
+  const [followers, setFollowers] = useState(mockFollowers)
 
   // request loading
   const [requests, setRequests] = useState(0)
-  const [loading, setisLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState({ show: false, mesg: '' })
 
   const searchGithubUser = async (user) => {
     toggleError()
-    // setLoading(true)
+    setIsLoading(true)
     const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
       console.log(err)
     )
-    console.log(response)
+
     if (response) {
       setGithubUser(response.data)
-      // more logic
+      const { login, followers_url } = response.data
+      // set state once all promise are complete for all
+      await Promise.allSettled([
+        axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+        axios(`${followers_url}?per_page=100`),
+      ])
+        .then((results) => {
+          const [repos, followers] = results
+          const status = 'fulfilled'
+          if (repos.status === status) {
+            setRepos(repos.value.data)
+          }
+          if (followers.status === status) {
+            setFollowers(followers.value.data)
+          }
+        })
+        .catch((err) => console.log(err))
     } else {
       toggleError(true, 'there is no user with that username')
     }
+    checkRequests()
+    setIsLoading(false)
   }
   // check rate
   const checkRequests = () => {
@@ -65,6 +83,7 @@ const GithubProvider = ({ children }) => {
         requests,
         error,
         searchGithubUser,
+        isLoading,
       }}
     >
       {children}
